72537a9327b9399cdca3eb148f93712a0c7a127c
KevinK <kevin.kihata@gmail.com>
2025-01-09 13:58:26 +0800

    rename utxos param to inputs

--- funding.d.ts
+++ funding.d.ts
@@ -1,7 +1,7 @@
 // funding.d.ts
 import { Target, UTXO } from "./index";
 
-export default function funding(utxos: UTXO[], outputs: Target[], feeRate: number): {
+export default function funding(inputs: UTXO[], outputs: Target[], feeRate: number): {
     funding: number,
     totalOutput: number,
     totalInput: number,
--- funding.js
+++ funding.js
@@ -1,21 +1,21 @@
 const utils = require('./utils')
 
-// given utxos, outputs and feerate, return sats needed to fund the transaction
-module.exports = function funding (utxos, outputs, feeRate) {
+// given inputs, outputs and feerate, return sats needed to fund the transaction
+module.exports = function funding (inputs, outputs, feeRate) {
   if (!isFinite(utils.uintOrNaN(feeRate))) return {}
 
   let inAccum = 0
   const outAccum = utils.sumOrNaN(outputs)
 
-  for (var i = 0; i < utxos.length; ++i) {
-    const utxo = utxos[i]
+  for (var i = 0; i < inputs.length; ++i) {
+    const utxo = inputs[i]
     const utxoValue = utils.uintOrNaN(utxo.value)
 
     inAccum += utxoValue
   }
 
-  // use all utxos as inputs
-  const bytesAccum = utils.transactionBytes(utxos, outputs)
+  // use all inputs and outputs
+  const bytesAccum = utils.transactionBytes(inputs, outputs)
   const fee = feeRate * bytesAccum
 
   return {
